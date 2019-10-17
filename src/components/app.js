8dc86639edb93bad39633f85a32672f4fd17d07b
bryanjlim <bryanlim@cs.washington.edu>
2019-10-16 19:56:10 -0700

    Add dialog to main page

--- src/components/app.js
+++ src/components/app.js
@@ -1,23 +1,72 @@
 import React from "react";
-import {Typography, Button, CssBaseline, withStyles} from '@material-ui/core'
+import {
+  Typography,
+  Button,
+  CssBaseline,
+  withStyles
+} from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
-const styles = (theme) => ({
+const styles = theme => ({
   text: {
-    color: 'blue',
-    textAlign: 'center'
+    color: "blue",
+    textAlign: "center"
   }
 });
 
-const App = (props) => {
-  const {classes} = props
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogOpen: false
+    };
+  }
 
-  return (
-    <div>
-      <CssBaseline/>
-      <Typography className={classes.text}>{props.text}</Typography>
-      <Button onClick={props.alert}>Test</Button>
-    </div>
-  );
-};
+  closeDialog = () => {
+    this.setState({dialogOpen: false})
+  }
+
+  render() {
+    const {classes} = this.props;
+
+    return (
+      <div>
+        <Dialog
+          open={this.state.dialogOpen}
+          onClose={this.closeDialog}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Use Google's location service?"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Let Google help apps determine location. This means sending
+              anonymous location data to Google, even when no apps are running.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.closeDialog} color="primary">
+              Disagree
+            </Button>
+            <Button onClick={this.closeDialog} color="primary" autoFocus>
+              Agree
+            </Button>
+          </DialogActions>
+        </Dialog>
+
+
+        <CssBaseline />
+        <Typography className={classes.text}>{this.props.text}</Typography>
+        <Button onClick={() => {this.setState({dialogOpen: true})}}>Open Dialog</Button>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 /* eslint-disable react/jsx-filename-extension */
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
-import blueGrey from '@material-ui/core/colors/blueGrey';
-import green from '@material-ui/core/colors/green';
-import createHistory from 'history/createBrowserHistory';
-import { SnackbarProvider } from 'notistack';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './components/app';
-import configureStore from './configureStore';
-import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
+import blueGrey from "@material-ui/core/colors/blueGrey";
+import green from "@material-ui/core/colors/green";
+import createHistory from "history/createBrowserHistory";
+import { SnackbarProvider } from "notistack";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./components/app";
+import configureStore from "./configureStore";
+import registerServiceWorker from "./registerServiceWorker";
 
 const initialState = {};
 const history = createHistory();
@@ -20,7 +20,7 @@ const theme = createMuiTheme({
   palette: {
     primary: blueGrey,
     secondary: green,
-    type: 'dark'
+    type: "dark"
   }
 });
 
@@ -28,12 +28,16 @@ ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider theme={theme}>
       <SnackbarProvider maxSnack={6}>
-        <App text="hi" alert={() => {
-          alert("Test Presed")
-        }}/>
+        <App
+          text="hi"
+          alert={() => {
+            alert("Hi");
+            alert("This is a test");
+          }}
+        />
       </SnackbarProvider>
     </MuiThemeProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 registerServiceWorker();
