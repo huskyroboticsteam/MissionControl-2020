2c6238b10f2c5d172e89b15b4070e31ab3527094
Alex Chan <chana98@uw.edu>
2020-02-04 19:55:56 -0800

    removed dummy graph

--- client/src/components/science-component.tsx
+++ client/src/components/science-component.tsx
@@ -3,24 +3,15 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { compose } from "recompose";
 import openSocket from "../actions/socket/openSocket";
-import LineChart from "react-linechart";
-import "../node_modules/react-linechart/dist/styles.css";
 
 type ScienceProps = {};
 class ScienceComponent extends React.Component<ScienceProps> {
   // this displays science data
   //copy pasted graph
   render() {
-    const data = [
-      {
-        color: "steelblue",
-        points: [{ x: 1, y: 2 }, { x: 3, y: 5 }, { x: 7, y: -3 }]
-      }
-    ];
     return (
-      <div className="App">
-        <h1>My First LineChart</h1>
-        <LineChart width={600} height={400} data={data} />
+      <div>
+        <h1>this is science</h1>
       </div>
     );
   }
