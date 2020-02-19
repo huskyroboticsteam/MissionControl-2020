4ab7cbf2b18ad88732b0a665d5e5d051f71049a5
Konda Varsha <vkonda@uw.edu>
2020-02-18 18:39:38 -0800

    Temperature component (#7)
    * Send JSON commands recieved from client
    
    * added temperature component
    
    * added temperature-component style
    
    * styled and fixed temperature ui
    
    Co-authored-by: Bryan Lim <bryanlim@cs.washington.edu>

--- client/src/components/dashboard.tsx
+++ client/src/components/dashboard.tsx
@@ -4,7 +4,7 @@ import ReactSpeedometer from "react-d3-speedometer";
 import StopButton from "./stop-button";
 import ConnectionQualityComponent from "./connection-quality-component";
 import "./dashboard.css";
-
+import Temperature from "./temperature-component";
 import DataPacket from "../types";
 type DashProps = {
   sensors: DataPacket;
@@ -12,34 +12,35 @@ type DashProps = {
 // this is the dash
 // clean up style code later
 class DashComponent extends React.Component<DashProps> {
-    render() {
-        return ( 
-            <div style={{ background:"#959298", height:"250px"}}>
-                <div style = {{float:"left", padding: "10px", margin: "40px"}}>
-                <StopButton/> 
-                </div>
-                <div style = {{float:"left", marginTop: "30px", margin: "30px"}}>
-                <ReactSpeedometer
-                maxValue={500}
-                value={473}
-                needleColor="yellow"
-                startColor="green"
-                maxSegmentLabels={5}
-                segments={10}
-                endColor="blue"
-                />
-                </div>
-                <div style = {{float:"left", background: "#F3F5AD", padding: "15px", margin: "40px"}}>
-                <p>
-                    X: 163.45 <br/> <br/> Y: -88.39 <br/> <br/> Z: 5.55
-                </p>
-                </div>  
-                <div style = {{float:"right", padding: "1px", margin: "30px"}}>
-                <ConnectionQualityComponent quality={100}/>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div style={{ background: "#959298", height: "250px" }}>
+        <div style={{ float: "left", padding: "10px", margin: "40px" }}>
+          <StopButton />
+        </div>
+        <div style={{ float: "left", marginTop: "30px", margin: "30px" }}>
+          <ReactSpeedometer
+            maxValue={500}
+            value={473}
+            needleColor="yellow"
+            startColor="green"
+            maxSegmentLabels={5}
+            segments={10}
+            endColor="blue"
+          />
+        </div>
+        <div style = {{float:"left", background: "#F3F5AD", padding: "15px", margin: "40px"}}>
+        <p>
+          X: 163.45 <br /> <br /> Y: -88.39 <br /> <br /> Z: 5.55
+        </p>
+        </div>
+        <div style={{ float: "right", padding: "1px", margin: "30px" }}>
+          <ConnectionQualityComponent quality={100} />
+          <Temperature value = {this.props.sensors.temperature}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 
---
+++ client/src/components/temperature-component.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useRef } from "react";
+import SvgGauge from "svg-gauge";
+import "./temperature-component.css";
+const defaultOptions = {
+  animDuration: 1,
+  showValue: true,
+  initialValue: 0,
+  max: 200 ,
+  dialStartAngle: 180,
+  dialEndAngle: 0,
+  color: function(value) {
+    if(value < 50) {
+      return "#5ee432";
+    }else if(value < 100) {
+      return "#fffa50";
+    }else if(value < 150) {
+      return "#f7aa38";
+    }else {
+      return "#ef4655";
+    }
+  } 
+};
+
+const Temperature = props => {
+  const gaugeEl = useRef(null);
+  const gaugeRef = useRef(null);
+  useEffect(() => {
+    if (!gaugeRef.current) {
+      const options = { ...defaultOptions, ...props };
+      gaugeRef.current = SvgGauge(gaugeEl.current, options);
+      gaugeRef.current.setValue(options.initialValue);
+    }
+    gaugeRef.current.setValueAnimated(props.value, 1);
+  }, [props]);
+
+  return <div ref={gaugeEl} className="gauge-container two"><div className="value-text">°F</div></div>;
+};
+
+export default Temperature;
