39df4714ecaa85ef83ba7ec318db4b3cadaadfc1
karansingh098 <karanvsingh520@gmail.com>
2021-04-03 13:54:57 -0700

    added color

--- client/src/components/dash-components/stop-status.tsx
+++ client/src/components/dash-components/stop-status.tsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { RoverCommands } from '../../rover-commands';
 
 class StopStatus extends React.Component<{}, {}> {
-
+    
     render() {
         if (RoverCommands.isEStopped()) {
             return (
                 <div>
-                    <h3>Status: <span style={/*FILL*/}>E-stop engaged</span></h3>
+                    <h3>Status: <span style={{ color: "red" }}>E-stop engaged</span></h3>
                     <p>Press shift-space to disengage E-stop</p>
                 </div>
             );
         } else {
             return (
                 <div>
-                    <h3>Status: <span style={/*FILL*/}>operational</span></h3>
+                    <h3>Status: <span style={{ color: "green" }}>operational</span></h3>
                     <p>Press shift-space to disengage E-stop</p>
                 </div>
             );
